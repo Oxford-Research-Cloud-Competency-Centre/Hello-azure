2ff80292fed0f128c614dd6423b586b2871d7a9b
Paul Demanze <keiran1337@gmail.com>
2025-01-30 16:34:23 +0000

    fix notice

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# © Chancellors, Masters and Scholars of The University of Oxford. All rights reserved.
+# © The Chancellor, Masters and Scholars of The University of Oxford. All rights reserved.
 
 gunicorn --bind=0.0.0.0:443 app:app
